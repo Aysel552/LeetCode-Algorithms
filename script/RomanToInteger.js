5ff8892c4ac20faf010016f9a49ca4333a16bb7a
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-05-16 22:26:23 +0400

    corrected the solution of RomanToInteger

--- script/RomanToInteger.js
+++ script/RomanToInteger.js
@@ -7,47 +7,34 @@ const romanNumbers = {
     D: 500,
     M: 1000
 }
-  
-  const s = "MXLLIXCM"
-var romanToInt = function(s) {
-    let converter = 0;
-    
-    for(let i=0; i <s.length; i++){
-    if(s[i] === 'I' && s[i+1] === 'V'){
-        converter+=4;
-        i++;
+
+// console.log(romanNumbers);
+
+
+var romanToInt = function (s) {
+    const array = s.split('');
+
+    let total = 0;
+    let current;
+    let currentValue;
+    let next;
+    let nextValue;
+
+    for (let i = 0; i < array.length; i++){
+        current = array[i]
+        currentValue = romanNumbers[current]
+
+        next = array[i + 1]
+        nextValue = romanNumbers[next]
+
+
+        if (currentValue < nextValue) {
+            total-= currentValue
+        }
+
+        else {
+            total+= currentValue
+        }
     }
-        
-        else if (s[i] === 'I' && s[i+1] === 'X') {
-          converter+=9;
-          i++  
-        }
-        
-        else if(s[i] === "X" && [s+1] === "L"){
-            converter+=40
-            i++
-        }
-        
-        else if(s[i] === "X" && [s+1] === "C"){
-            converter+=90
-            i++
-        }
-        
-        else if(s[i] === "C" && [s+1] === "D"){
-            converter+=400
-            i++
-        }
-        
-        else if(s[i] === "C" && [s+1] === "M"){
-            converter+=900
-            i++
-        }
-        
-        else{
-            converter+= romanNumbers[s[i]]
-        }
-        }
-    
-        return converter;
-    
- };
+    return total
+}
