567fcb795f3dc93e83c3278a627892ee405db62a
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-05-27 21:56:27 +0400

    solved remove duplicates

---
+++ script/Remove Duplicates from Sorted Array.js
@@ -0,0 +1,15 @@
+var removeDuplicates = function(nums) {
+    if(nums.length === 0)
+        return 0
+    let i=0
+    let j = 1
+    while(j<nums.length){
+        if(nums[j] !== nums[i]){
+            i++
+            nums[i] = nums[j]
+            j++
+        } else j++
+    }
+    return i+1
+    
+};
